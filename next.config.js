664722195da6eae6368e32ccab29c17eb69e0cb4
Janu7878 <119414805+Janu7878@users.noreply.github.com>
2022-12-27 22:50:04 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,11 @@
-
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/authors/maria-thaituan',
+        destination: '/categories/archeology',
+        permanent: true,
+      },
+    ]
+  },
+}
