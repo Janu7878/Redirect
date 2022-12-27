0a837b55dcde2049e1b32c67bf0e2a82f6e4a554
Janu7878 <119414805+Janu7878@users.noreply.github.com>
2022-12-28 00:02:24 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -26,7 +26,7 @@ const nextConfig = {
 				source: "/posts/:slug",
 				destination: "https://fancy4work.com/:slug",
 				basePath: false,
-				permanent: false,
+				permanent: true,
 			},
 			{
 				source: "/youtube",
